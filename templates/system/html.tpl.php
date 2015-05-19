984575c153d41280313384960229961eca440141
Axa Mei Liauw <aliauw@princeton.edu>
2015-05-19 13:21:58 -0400

    Update base theme

--- templates/system/html.tpl.php
+++ templates/system/html.tpl.php
@@ -65,14 +65,12 @@
   <?php print $page; ?>
   <?php print $page_bottom; ?>
   <!-- JS for Chat Widget. -->
-  <script 
-   type="text/javascript"> 
+  <script type="text/javascript"> 
     (function() {
       var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
       x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "libraryh3lp.com/js/libraryh3lp.js?multi,poll";
       var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
     })();
   </script>
-
 </body>
 </html>
